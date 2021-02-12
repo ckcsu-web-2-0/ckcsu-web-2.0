e686ff96e76f6417145e634078d8b120c538d1e7
Ebolaemperor <627702281@qq.com>
2021-02-12 12:51:17 +0800

    Add birthday reminder 0.3.1
    修了一点小锅

--- themes/ckc/source/js/birthday.js
+++ themes/ckc/source/js/birthday.js
@@ -3,33 +3,34 @@ var birthday_cli_on = document.getElementById("birthday_on"); //on为按钮
 var birthday_flag = false, birthday_timer = null, birthday_initime = null, birthday_r_len = 0;
 
 if(birthday_menubox.style.right=== 0){
- birthday_flag = true;
- }
-else{
- birthday_flag = false;
- }
+    birthday_flag = true;
+} else {
+    birthday_flag = false;
+}
+
 birthday_cli_on.onclick = function () {
- //为on按钮绑定click事件
- clearTimeout(birthday_initime);
- //根据状态birthday_flag执开展开收缩
- if (birthday_flag) {
-  birthday_r_len = 0;
-  birthday_timer = setInterval(birthday_slideright, 10);
- } else {
-  birthday_r_len = -260;
-  birthday_timer = setInterval(birthday_slideleft, 10);
- }
+    //为on按钮绑定click事件
+    clearTimeout(birthday_initime);
+    //根据状态birthday_flag执开展开收缩
+    if (birthday_flag) {
+        birthday_r_len = 0;
+        birthday_timer = setInterval(birthday_slideright, 10);
+    } else {
+        birthday_r_len = -260;
+        birthday_timer = setInterval(birthday_slideleft, 10);
+    }
 }
 
 var curDate = new Date();
+var curDay = new Date(curDate.toDateString() + " 00:00:00");
 
 const getDaysDif = (dateInitial, dateFinal) =>
-  (dateFinal - dateInitial) / (1000 * 3600 * 24);
+    (dateFinal - dateInitial) / (1000 * 3600 * 24);
 
 function ckcPerson(name, depart, birthMonth, birthDay){
     this.name = name;
     this.depart = depart;
-    this.birthday = new Date(curDate.getFullYear().toString() + "-" + birthMonth + "-" + birthDay);
+    this.birthday = new Date(curDate.getFullYear().toString() + "-" + birthMonth + "-" + birthDay + " 00:00:00");
 }
 
 var allPerson = new Array(145);
@@ -180,9 +181,9 @@ allPerson[143] = new ckcPerson("周陈昊", "外交", 3, 14);
 allPerson[144] = new ckcPerson("王子奇", "外交", 12, 24);
 
 for(var i = 0; i < allPerson.length; i++){
-    var restDay = getDaysDif(curDate, allPerson[i].birthday);
+    var restDay = getDaysDif(curDay, allPerson[i].birthday);
     if(restDay >= 0 && restDay < 15){
-        outputBirth("距 " + allPerson[i].depart + " " + allPerson[i].name + " 的生日还有 " + parseInt(restDay) + "天！");
+        outputBirth("距 " + allPerson[i].depart + " " + allPerson[i].name + " 的生日还有 " + restDay + "天！");
     }
 }
 
@@ -195,23 +196,24 @@ function outputBirth(birthText){
 
 //展开
 function birthday_slideright() {
- if (birthday_r_len <= -260) {
-  clearInterval(birthday_timer);
-  birthday_flag = !birthday_flag;
-  return false;
- }else{
-  birthday_r_len -= 5;
-  birthday_menubox.style.right = birthday_r_len + 'px';
- }
+    if (birthday_r_len <= -260) {
+        clearInterval(birthday_timer);
+        birthday_flag = !birthday_flag;
+        return false;
+    } else {
+        birthday_r_len -= 5;
+        birthday_menubox.style.right = birthday_r_len + 'px';
+    }
 }
+
 //收缩
-function birthday_slideleft() {
- if (birthday_r_len >= 0) {
-  clearInterval(birthday_timer);
-  birthday_flag = !birthday_flag;
-  return false;
- } else {
-  birthday_r_len += 5;
-  birthday_menubox.style.right = birthday_r_len + 'px';
- }
+function birthday_slideleft(){
+    if (birthday_r_len >= 0) {
+        clearInterval(birthday_timer);
+        birthday_flag = !birthday_flag;
+        return false;
+    } else {
+        birthday_r_len += 5;
+        birthday_menubox.style.right = birthday_r_len + 'px';
+    }
 }
